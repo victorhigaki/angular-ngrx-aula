f50464e4b92c0c7a41cd12c8ab31ad295a47d7c6
Victor Higaki <victorhigaki@outlook.com>
2024-03-03 17:42:06 -0300

    feat: create livro reducer

--- src/livros/livro.reducer.ts
+++ src/livros/livro.reducer.ts
@@ -1,4 +1,6 @@
+import { createReducer, on } from "@ngrx/store";
 import { Livro } from "./livro.model";
+import { livroActions } from "./livro.actions";
 
 enum LivroStatus {
     loading = 'loading',
@@ -12,3 +14,30 @@ export interface LivroState {
     error: '' | null,
     status: LivroStatus
 }
+
+const initialState: LivroState = {
+    error: null,
+    status: LivroStatus.pending,
+    livros: []
+}
+
+export const LivroReducer = createReducer(
+    initialState,
+    on(livroActions.carregarLivros, (stateAtual) => {
+        return {
+            ...stateAtual,
+            livros: livrosIniciais,
+            status: LivroStatus.success
+        }
+    })
+)
+
+const livrosIniciais = [
+    {
+        id: 1,
+        nome: 'Harry Potter'
+    }, {
+        id: 2,
+        nome: 'Senhor dos Aneis'
+    }
+]
