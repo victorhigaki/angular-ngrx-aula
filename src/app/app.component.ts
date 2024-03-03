408d835c76a9dfb31e5332b72276f2047d863591
Victor Higaki <victorhigaki@outlook.com>
2024-03-03 17:20:05 -0300

    feat: obter livros observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,24 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Livro } from '../livros/livro.model';
 import { LivroService } from '../livros/livro.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
   title = 'angular-ngrx-aula';
 
   livroService = inject(LivroService);
-  livros: Livro[] = [];
+  //livros: Livro[] = [];
+  livros$ = this.livroService.obterLivrosApi();
 
   ngOnInit(): void {
-    this.livros = this.livroService.obterLivros();
+    // this.livros = this.livroService.obterLivros();
   }
 }
